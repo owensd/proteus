bc5c0f09a09ed6acb3068c5b2805d0a49a9ab054
David Owens II <david@owensd.io>
2015-09-03 22:35:05 -0700

    Initial setup of the Proteus project.

---
+++ src/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Proteus
+//
+//  Created by David Owens on 9/3/15.
+//  Copyright © 2015 owensd.io. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ tests/lexer/BaselineTests.swift
@@ -0,0 +1,13 @@
+//
+//  lexer_tests.swift
+//  lexer_tests
+//
+//  Created by David Owens on 9/3/15.
+//  Copyright © 2015 owensd.io. All rights reserved.
+//
+
+import XCTest
+
+class BaselineTests: XCTestCase {
+    
+}
